4363d8e4fca8379cf8620492af51f75ccda0c3e3
yukiko bass <springcomesagain129@gmail.com>
2019-05-14 14:58:51 +0900

    init

---
+++ src/main/java/com/kintaitool/kintaitool/KintaitoolApplication.java
@@ -0,0 +1,13 @@
+package com.kintaitool.kintaitool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KintaitoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KintaitoolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kintaitool/kintaitool/KintaitoolApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kintaitool.kintaitool;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KintaitoolApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
